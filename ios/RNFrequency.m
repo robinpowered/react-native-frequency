3ec37bed5733c2022a93c4c6399d23445167db5d
Ying Hang Eng <eyinghang@gmail.com>
2018-01-03 16:48:17 -0500

    code changes and cleanup

--- ios/RNFrequency.m
+++ ios/RNFrequency.m
@@ -1,65 +1,20 @@
 #import "RNFrequency.h"
 #import "TGSineWaveToneGenerator.h"
 
-// Used to send events to JS
-#if __has_include(<React/RCTBridge.h>)
-#import <React/RCTBridge.h>
-#elif __has_include("RCTBridge.h")
-#import "RCTBridge.h"
-#else
-#import "React/RCTBridge.h"
-#endif
-
-#if __has_include(<React/RCTEventDispatcher.h>)
-#import <React/RCTEventDispatcher.h>
-#elif __has_include("RCTEventDispatcher.h")
-#import "RCTEventDispatcher.h"
-#else
-#import "React/RCTEventDispatcher.h"
-#endif
-
 @interface RNFrequency ()
 @property(strong) TGSineWaveToneGenerator *toneGenRef;
 @end
 
 @implementation RNFrequency
 
-@synthesize bridge = _bridge;
-
-static NSString * const AUDIO_CHANGED_NOTIFICATION = @"AUDIO_CHANGED_NOTIFICATION";
+static UInt32 const TWO_CHANNELS = 2;
 
 - (instancetype)init
 {
     if (self = [super init]) {
-        self.toneGenRef = [[TGSineWaveToneGenerator alloc] initWithChannels:2];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(audioRouteChangeListenerCallback:) name:AVAudioSessionRouteChangeNotification object:nil];
+        self.toneGenRef = [[TGSineWaveToneGenerator alloc] initWithChannels:TWO_CHANNELS];
     }
     return self;
-}
-
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (void)audioRouteChangeListenerCallback:(NSNotification*)notification
-{
-    [_bridge.eventDispatcher sendDeviceEventWithName:AUDIO_CHANGED_NOTIFICATION
-      body:(@{
-        @"audioJackPluggedIn": @([RNFrequency isAudioJackInUse])
-      })];
-}
-
-+ (BOOL) isAudioJackInUse
-{
-    AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
-
-    for (AVAudioSessionPortDescription* desc in [route outputs]) {
-        if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones])
-            return YES;
-    }
-
-    return NO;
 }
 
 - (dispatch_queue_t)methodQueue
@@ -69,11 +24,6 @@ static NSString * const AUDIO_CHANGED_NOTIFICATION = @"AUDIO_CHANGED_NOTIFICATIO
 
 RCT_EXPORT_MODULE()
 
-- (NSDictionary *)constantsToExport
-{
-    return @{ @"AUDIO_CHANGED_NOTIFICATION": AUDIO_CHANGED_NOTIFICATION };
-}
-
 RCT_EXPORT_METHOD(playFrequency:(double)frequency duration:(double)duration resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
@@ -81,12 +31,6 @@ RCT_EXPORT_METHOD(playFrequency:(double)frequency duration:(double)duration reso
     self.toneGenRef->_channels[0].frequency=frequency;
     [self.toneGenRef playForDuration:duration];
     resolve(@YES);
-}
-
-RCT_EXPORT_METHOD(isAudioJackPluggedIn:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
-    resolve(@([RNFrequency isAudioJackInUse]));
 }
 
 @end
