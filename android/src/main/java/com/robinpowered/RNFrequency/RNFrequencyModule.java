76de9a5c40e75067d424797db7030610f160c147
Ying Hang Eng <eyinghang@gmail.com>
2018-01-05 18:50:32 -0500

    fix android issues

--- android/src/main/java/com/robinpowered/RNFrequency/RNFrequencyModule.java
+++ android/src/main/java/com/robinpowered/RNFrequency/RNFrequencyModule.java
@@ -21,77 +21,106 @@ import android.media.AudioDeviceInfo;
 import android.os.Build;
 
 public class RNFrequencyModule extends ReactContextBaseJavaModule {
-  private static final String MODULE_NAME = "RNFrequency";
-  private AudioTrack track;
+    private static final String MODULE_NAME = "RNFrequency";
+    private static final String TRACK_STOPPED_PLAYING = "TRACK_STOPPED_PLAYING";
+    private static final int SAMPLE_RATE = 44100;
 
-  public RNFrequencyModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    private AudioTrack track;
+    private Promise promise;
 
-  @Override
-  public String getName() {
-    return MODULE_NAME;
-  }
-
-  @ReactMethod
-  public void playFrequency(double frequency, double duration, final Promise promise) {
-    final int dur = (int) duration;
-    final int freq = (int) frequency;
-
-    // ~1 to make sure count is an even number
-    // *2 for stereo
-    int count = (int)(11025.0 * 2.0 * (dur)) & ~1;
-
-    // 0x7FFF to convert 32b to 16b
-    short[] samples = new short[count];
-    for(int i = 0; i < count; i += 2){
-      short sample = (short)(Math.sin(2 * Math.PI * i / (44100.0 / freq)) * 0x7FFF);
-      samples[i + 0] = sample;
-      samples[i + 1] = sample;
+    public RNFrequencyModule(ReactApplicationContext reactContext) {
+        super(reactContext);
     }
 
-    // If track is initialized, stop playback and release resources
-    if (track != null && track.getState() != AudioTrack.STATE_UNINITIALIZED) {
-        if (track.getPlayState() != AudioTrack.PLAYSTATE_STOPPED) {
-            try{
-                track.stop();
-            }catch (IllegalStateException e)
-            {
+    private void stop() {
+        // If track is initialized, stop playback and release resources
+        try {
+            track.stop();
+        } catch (IllegalStateException e) {
+            // no-op
+        }
+
+        track.release();
+        track = null;
+
+        promise.reject(TRACK_STOPPED_PLAYING, "Track stopped playing");
+        promise = null;
+    }
+
+    private void saveAudioTrackAndPromise(AudioTrack audioTrack, Promise promise) {
+        track = audioTrack;
+        this.promise = promise;
+    }
+
+    @Override
+    public String getName() {
+        return MODULE_NAME;
+    }
+
+    @ReactMethod
+    public void playFrequency(double frequency, double duration, final Promise promise) {
+        if (track != null
+            && track.getState() != AudioTrack.STATE_UNINITIALIZED
+            && track.getPlayState() != AudioTrack.PLAYSTATE_STOPPED
+        ) {
+          stop();
+        }
+
+        final int dur = (int) duration;
+        final int numOfSamples = dur * SAMPLE_RATE;
+
+        final double sample[] = new double[numOfSamples];
+        final byte soundData[] = new byte[2 * numOfSamples];
+
+        for (int i = 0; i < numOfSamples; ++i) {
+         sample[i] = Math.sin(2 * Math.PI * i / (SAMPLE_RATE/frequency));
+        }
+
+        int idx = 0;
+        for (double dVal : sample) {
+         short val = (short) (dVal * 32767);
+         soundData[idx++] = (byte) (val & 0x00ff);
+         soundData[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+
+        AudioTrack freqTrack;
+
+        // create audio track - methods used differ based on OS version
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            freqTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, numOfSamples, AudioTrack.MODE_STATIC);
+        } else {
+            freqTrack = new AudioTrack.Builder()
+                .setAudioFormat(new AudioFormat.Builder()
+                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                    .setSampleRate(SAMPLE_RATE)
+                    .setChannelMask(AudioFormat.CHANNEL_OUT_MONO).build())
+                .setBufferSizeInBytes(numOfSamples).build();
+        }
+
+        // push sample into AudioTrack object
+        freqTrack.write(soundData, 0, numOfSamples);
+
+        // callback when track finishes playing
+        freqTrack.setNotificationMarkerPosition(numOfSamples / 2);
+
+        freqTrack.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
                 // no-op
             }
 
-        }
-        track.release();
-    }
-
-    // create audio track - methods used differ based on OS version
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-        track = new AudioTrack(AudioManager.STREAM_MUSIC, 11025,
-        AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
-        count * (Short.SIZE / 8), AudioTrack.MODE_STATIC);
-    } else {
-        track = new AudioTrack.Builder()
-            .setAudioFormat(new AudioFormat.Builder()
-                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                .setSampleRate(11025)
-                .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
-                .build())
-            .setBufferSizeInBytes(count * (Short.SIZE / 8))
-            .build();
-    }
-
-    // push sample into AudioTrack object
-    track.write(samples, 0, count);
-
-    // play track
-    track.play();
-
-    new android.os.Handler().postDelayed(
-        new Runnable() {
-            public void run() {
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+                track.release();
+                track = null;
                 promise.resolve(true);
             }
-        },
-    (long) (duration * 1000));
-  }
+        });
+
+        saveAudioTrackAndPromise(freqTrack, promise);
+
+        // play track
+        freqTrack.play();
+    }
 }
