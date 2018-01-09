b6a137aaf55c18872b6a0f60930d803896120717
Ying Hang Eng <eyinghang@gmail.com>
2018-01-08 20:06:38 -0500

    reimplement class

--- android/src/main/java/com/robinpowered/RNFrequency/RNFrequencyModule.java
+++ android/src/main/java/com/robinpowered/RNFrequency/RNFrequencyModule.java
@@ -11,18 +11,104 @@ import android.media.AudioTrack;
 import android.os.Build;
 
 public class RNFrequencyModule extends ReactContextBaseJavaModule {
-    private static final String MODULE_NAME = "FrequencyManager";
-    private static final String TRACK_STOPPED_PLAYING = "TRACK_STOPPED_PLAYING";
+    private static final String MODULE_NAME = "RNFrequency";
     private static final int SAMPLE_RATE = 44100;
 
-    private AudioTrack track;
-    private Promise promise;
+    private AudioSession audioSession;
 
     public RNFrequencyModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
-    private void stop() {
+    @Override
+    public String getName() {
+        return MODULE_NAME;
+    }
+
+    private void cancel() {
+        audioSession.cancel();
+        audioSession = null;
+    }
+
+    private void complete() {
+        audioSession.complete();
+        audioSession = null;
+    }
+
+    @ReactMethod
+    public void playFrequency(double frequency, double duration, final Promise promise) {
+        if (audioSession != null && audioSession.isPlaying()) {
+            cancel();
+        }
+
+        final int dur = (int) duration;
+        final int numOfSamples = dur * SAMPLE_RATE;
+
+        final double sample[] = new double[numOfSamples];
+        final byte soundData[] = new byte[2 * numOfSamples];
+
+        for (int i = 0; i < numOfSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (SAMPLE_RATE/frequency));
+        }
+
+        int idx = 0;
+        for (double dVal : sample) {
+            short val = (short) (dVal * 32767);
+            soundData[idx++] = (byte) (val & 0x00ff);
+            soundData[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+
+        AudioTrack track;
+
+        // create audio track - methods used differ based on OS version
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            track = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, numOfSamples, AudioTrack.MODE_STATIC);
+        } else {
+            track = new AudioTrack.Builder()
+                    .setAudioFormat(new AudioFormat.Builder()
+                            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                            .setSampleRate(SAMPLE_RATE)
+                            .setChannelMask(AudioFormat.CHANNEL_OUT_MONO).build())
+                    .setBufferSizeInBytes(numOfSamples).build();
+        }
+
+        // push sample into AudioTrack object
+        track.write(soundData, 0, numOfSamples);
+
+        // callback when track finishes playing
+        track.setNotificationMarkerPosition(numOfSamples / 2);
+
+        track.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
+                // no-op
+            }
+
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+                RNFrequencyModule.this.complete();
+            }
+        });
+
+        audioSession = new AudioSession(track, promise);
+
+        // play track
+        track.play();
+    }
+}
+
+class AudioSession {
+    Promise promise;
+    AudioTrack track;
+    static final String TRACK_STOPPED_PLAYING = "TRACK_STOPPED_PLAYING";
+
+    AudioSession(AudioTrack track, Promise promise) {
+        this.track = track;
+        this.promise = promise;
+    }
+
+    void cancel() {
         // If track is initialized, stop playback and release resources
         try {
             track.stop();
@@ -33,87 +119,21 @@ public class RNFrequencyModule extends ReactContextBaseJavaModule {
         track.release();
         track = null;
 
-        if (promise !=  null) {
-            promise.reject(TRACK_STOPPED_PLAYING, "Track stopped playing");
-            promise = null;
-        }
+        promise.reject(TRACK_STOPPED_PLAYING, "Track stopped playing");
+        promise = null;
     }
 
-    private void saveAudioTrackAndPromise(AudioTrack audioTrack, Promise promise) {
-        track = audioTrack;
-        this.promise = promise;
+    void complete() {
+        track.release();
+        track = null;
+
+        promise.resolve(true);
+        promise = null;
     }
 
-    @Override
-    public String getName() {
-        return MODULE_NAME;
-    }
-
-    @ReactMethod
-    public void playFrequency(double frequency, double duration, final Promise promise) {
-        if (track != null &&
-            track.getState() != AudioTrack.STATE_UNINITIALIZED &&
-            track.getPlayState() != AudioTrack.PLAYSTATE_STOPPED
-        ) {
-            stop();
-        }
-
-        final int dur = (int) duration;
-        final int numOfSamples = dur * SAMPLE_RATE;
-
-        final double sample[] = new double[numOfSamples];
-        final byte soundData[] = new byte[2 * numOfSamples];
-
-        for (int i = 0; i < numOfSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (SAMPLE_RATE / frequency));
-        }
-
-        int idx = 0;
-        for (double dVal: sample) {
-            short val = (short)(dVal * 32767);
-            soundData[idx++] = (byte)(val & 0x00ff);
-            soundData[idx++] = (byte)((val & 0xff00) >>> 8);
-        }
-
-        AudioTrack freqTrack;
-
-        // create audio track - methods used differ based on OS version
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            freqTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, numOfSamples, AudioTrack.MODE_STATIC);
-        } else {
-            freqTrack = new AudioTrack.Builder()
-                .setAudioFormat(new AudioFormat.Builder()
-                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                    .setSampleRate(SAMPLE_RATE)
-                    .setChannelMask(AudioFormat.CHANNEL_OUT_MONO).build())
-                .setBufferSizeInBytes(numOfSamples).build();
-        }
-
-        // push sample into AudioTrack object
-        freqTrack.write(soundData, 0, numOfSamples);
-
-        // callback when track finishes playing
-        freqTrack.setNotificationMarkerPosition(numOfSamples / 2);
-
-        freqTrack.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
-            @Override
-            public void onPeriodicNotification(AudioTrack track) {
-                // no-op
-            }
-
-            @Override
-            public void onMarkerReached(AudioTrack track) {
-                track.release();
-                track = null;
-                promise.resolve(true);
-                RNFrequencyModule.this.promise = null;
-            }
-        });
-
-        saveAudioTrackAndPromise(freqTrack, promise);
-
-        // play track
-        freqTrack.play();
+    boolean isPlaying() {
+        return track != null
+                && track.getState() != AudioTrack.STATE_UNINITIALIZED
+                && track.getPlayState() != AudioTrack.PLAYSTATE_STOPPED;
     }
 }
